bd13608a07d07e31247c2653d0c0ac2c400d42d0
patoliya <developer.patoliya@gmail.com>
2025-09-20 11:11:10 +0530

    initial backend setup

---
+++ face-recognition-attendance-system-api/src/server.js
@@ -0,0 +1,11 @@
+const app = require('./app');
+const dotenv = require('dotenv');
+
+// Load env variables
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
